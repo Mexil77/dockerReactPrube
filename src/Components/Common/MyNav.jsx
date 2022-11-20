17d8c5498955195140ef1c7a3f70f3f87f87c662
Emmanuel Garcia Torres <mendel_77@hotmail.com>
2022-11-20 12:34:31 +0100

    auth completed

--- src/Components/Common/MyNav.jsx
+++ src/Components/Common/MyNav.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./MyNav.css";
 
 export default function MyNav() {
+	const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
 	return (
 		<nav className="navbar">
 			<Link to={"/"} id={"logoNav"}>
 				<p>App Notes</p>
 			</Link>
-			<button className="btn">Login</button>
+			{isAuthenticated ? (
+				<div id="userSection">
+					<img src={user.picture} alt="" />
+					<button
+						className="btn"
+						onClick={() => {
+							logout({ returnTo: window.location.origin });
+						}}
+					>
+						LogOut
+					</button>
+				</div>
+			) : (
+				<button
+					className="btn"
+					onClick={() => {
+						loginWithRedirect();
+					}}
+				>
+					Login
+				</button>
+			)}
 		</nav>
 	);
 }
--- src/Components/Landing/Banner.jsx
+++ src/Components/Landing/Banner.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./Banner.css";
 
 export default function Banner() {
+	const { isAuthenticated, user } = useAuth0();
+
 	return (
 		<section>
 			<div className="Banner">
 				<div className="Banner_Box">
-					<h1>Bienvenido</h1>
+					<h1>Bienvenido {isAuthenticated && user.given_name}</h1>
 					<p>Tu app para gestionar todas tus notas</p>
-					<Link to={"/notes"}>
-						<button className="btn">Ir a notas</button>
-					</Link>
+					{isAuthenticated && (
+						<Link to={"/notes"}>
+							<button className="btn">Ir a notas</button>
+						</Link>
+					)}
 				</div>
 			</div>
 		</section>
